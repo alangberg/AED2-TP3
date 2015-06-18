32464bee8d24d6a298942f0bd06fdaf4bc56fbc3
alangberg <andreslangberg@gmail.com>
2015-06-18 19:11:55 -0300

    interfaz red

--- Red/Red.h
+++ Red/Red.h
@@ -1,36 +1,43 @@
-#include "TiposBasicos.h"
-#include "Lista.h"
+#ifndef RED_H_INCLUDED
+#define RED_H_INCLUDED
+
+#include ../Conjunto/"Conjunto.h"
+
 
 #include <ostream>
 
-namespace aed2
+namespace aed2;
 
-{
 
 template <typename T>
-class Conjunto
+class Red
 {
 	public:
-		/// Crea un conjunto vacío. (Operación Vacia())
-		Conjunto();
-		/// Crea por copia un conjunto (operación Copiar())
-		Conjunto(const Conjunto& otro);
+		/// Crea una red vacía. (Operación ArrancarRed())
+		Red();
+		/// Crea por copia una Red (operación Copiar())
+		Red(const Red& otro);
 		/// Destruye el conjunto, incluyendo los T alojados
-		~Conjunto();
+		~Red();
 		/// Operacion de asignacion
-		Conjunto<T>& operator=(const Conjunto<T>& otra);
-
+		Red<T>& operator=(const Red<T>& otra);
 		/// Operaciones básicas
-		void agregar(const T& elem);
-		//typename Lista<T>::Iterador agregar_rapido(const T& elem);
-		bool es_vacio() const;
-		bool pertenece(const T& elem);
-		//typename Lista<T>::Iterador eliminar(const T& elem);
-		Nat cardinal() const;
-		//void unir(Conjunto<T>& c1, const Conjunto<T> c2);
-		T& dameUno();
-		//void sinUno();
+		Conjunto<Pc> mostrarComputadoras();
+		void agregarCompu(const Pc& p);
+		void conectar(const Pc& p1, const Interfaz i1, const Pc& p2, const Interfaz i2);
+		bool estanConectadas(const Pc& p1, const Pc& p2);
+		Interfaz interfazQueUsan(const Pc& p1, const Pc& p2);
+		Conjunto<Pc> conectadoCon(const Pc& p1);
+		bool interfazUsada(const Pc& p1, const Interfaz i1);
+		Conjunto<Lista<Pc>> caminosMinimos(const Pc& p1, const Pc& p2);
+		bool existeCamino(const Pc& p1, const Pc& p2);
 
 	private:
-		Lista<T> l;
+        Dicc<Ip, Conjunto<Interfaz>> interfaces;
+        Dicc<Ip, Conjunto<Ip>> vecinos;
+        Lista<> conexiones; //FALTA COMPLETAR TUPLA PC,INTERFAZ,PC,INTERFAZ!!!!
+
 };
+
+
+#endif
