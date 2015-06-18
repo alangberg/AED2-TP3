01d03a8670096792c18f5a9c2df47512237fb1c5
juany360 <juaninv@outlook.com>
2015-06-18 19:27:08 -0300

    Aca esta el heap putos
    Mati gay

---
+++ Heap/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Heap.cpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+     ColaPriorHeap<int> a;
+
+
+
+    return log2(a.Cantidad()) == int(log2(a.Cantidad()));
+}
